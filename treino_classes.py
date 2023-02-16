77d9bec38658d0be33a962836d51f493f6b665c9
Ygsantos <yurigabriel924@gmail.com>
2023-02-16 19:14:15 -0400

    ...

--- treino_classes.py
+++ treino_classes.py
@@ -5,6 +5,7 @@ class Conta:
         self.__titular = titular # O __ é um aviso para o desenvolvedor saber que nao deve manusear o atributo fora da classe
         self.__saldo = saldo # O __ é um aviso para o desenvolvedor saber que nao deve manusear o atributo fora da classe
         self.__limite = limite # O __ é um aviso para o desenvolvedor saber que nao deve manusear o atributo fora da classe
+        self.__codigo_banco = '001'
 
     def extrato(self):
         print(f'O Saldo da sua conta é:"{self.saldo} {self.titular}')
@@ -21,6 +22,18 @@ class Conta:
             self.saldo -= valor
         else:
             print(f'O seu valor {valor} passou o limite permitido')
+
+    def transfere(self, valor, destino): #(Self,valor,origem, destino). Elimina a origem e mantem somente self e destino com o valor. evita redundancia
+        self.saca(valor) # eliminando a origem mantem apenas o self pois vai apontar para a conta de origem que esta usando o metodo.
+        destino.deposita(valor) # destino executando o metodo deposita com o valor
+        #conta2.transfere(100,conta1)
+        #encapsulado operacao criando um metodo na classe Conta.
+        #organizacao, legibilidade e manutencao do codigo.
+
+    # def inadimplente(self):
+    # Cuidado ao criar um metodo que nao pertence a essa classe, inadimplencia seria um metodo que nao esta na classe /Conta/
+    # Manter coesao do paradigma orientado a objeto.
+    # SOLID
     '''
     def get_saldo(self):    #retornar o valor do atributo
         return self.__saldo #atributo sendo manipulado dentro da classe.
@@ -33,7 +46,8 @@ class Conta:
 
     def get__numero(self): #retornar o valor do atributo
         return self.__numero #atributo sendo manipulado dentro da classe.
-'''
+    def set__limite(self, limite): #set de valor do atributo.
+        self.__limite = limite #atributo sendo manipulado dentro da classe.'''
     @property
     def saldo(self):
         return self.__saldo
@@ -54,20 +68,15 @@ class Conta:
         # setters
         # executa o comando como se estivesse acessando o atributo porém esta usando o metodo
         # definir primeiro o getter depois o setter
-    def transfere(self, valor, destino): #(Self,valor,origem, destino). Elimina a origem e mantem somente self e destino com o valor. evita redundancia
-        self.saca(valor) # eliminando a origem mantem apenas o self pois vai apontar para a conta de origem que esta usando o metodo.
-        destino.deposita(valor) # destino executando o metodo deposita com o valor
-        #conta2.transfere(100,conta1)
-        #encapsulado operacao criando um metodo na classe Conta.
-        #organizacao, legibilidade e manutencao do codigo.
 
-    def set__limite(self, limite): #set de valor do atributo.
-        self.__limite = limite #atributo sendo manipulado dentro da classe.
+    @property
+    def codigo_banco(self):
+        return self.__codigo_banco
 
-    #def inadimplente(self):
-    #Cuidado ao criar um metodo que nao pertence a essa classe, inadimplencia seria um metodo que nao esta na classe /Conta/
-    # Manter coesao do paradigma orientado a objeto.
-    #SOLID
+
+
+
+
 
 
 
